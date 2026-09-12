469a3b3462c9dbaf02c01581fdf3e971a6b8ea3a
Dave Anthony Garcia <dgarcia123david@gmail.com>
2026-09-11 22:00:11 -0500

    Updated CORS for Production - deleted the slash at the end

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(helmet());
 // Add your Netlify URL here after you deploy the frontend
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://my-database-app.netlify.app/",
+  "https://my-database-app.netlify.app",
 ];
 
 app.use(
